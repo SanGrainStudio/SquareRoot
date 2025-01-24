6fc1578fa6ce1a530506f8a41f780e65048e513c
SanGrainStudio <giedriusgurevicius@gmail.com>
2025-01-24 02:48:50 +0200

    Testing center of mass

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+
+    public GameObject fpsCam ;
+
+    public GameObject projectile;
+
+    public Transform barrelEnd;
+
+    public float projectileVelocity = 500;
+    public float range = 100;
+    public float damage = 50;
+
+    public bool RaycastOnly = true;
+
+    
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            ShootRay();
+            if(!RaycastOnly)
+            {
+                shootProjectile();
+            }
+            
+        }
+    }
+
+
+    void ShootRay()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward,out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+                if(RaycastOnly)
+                {
+                    target.GetComponent<Rigidbody>().AddForce(transform.forward * projectileVelocity,ForceMode.Impulse);
+                }
+                
+            }
+
+        }
+    }
+
+        void shootProjectile()
+    {
+        GameObject ball = Instantiate(projectile, barrelEnd.transform.position, transform.rotation);
+        ball.GetComponent<Rigidbody>().AddForce(transform.forward * projectileVelocity,ForceMode.Impulse);
+    }
+
+
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+
+    public float health = 100;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
