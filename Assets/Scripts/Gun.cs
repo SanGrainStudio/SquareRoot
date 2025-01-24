3ece4597a610c862ef660c7aec586596d2dbeb4a
SanGrainStudio <giedriusgurevicius@gmail.com>
2025-01-24 18:34:46 +0200

    Cleaned up for Working with meshes

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,16 +4,11 @@ public class Gun : MonoBehaviour
 {
 
 
-    public GameObject fpsCam ;
 
     public GameObject projectile;
-
-    public Transform barrelEnd;
-
     public float projectileVelocity = 500;
     public float range = 100;
     public float damage = 50;
-
     public bool RaycastOnly = true;
 
     
@@ -42,7 +37,7 @@ public class Gun : MonoBehaviour
     {
         RaycastHit hit;
 
-        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward,out hit, range))
+        if(Physics.Raycast(transform.position, transform.forward,out hit, range))
         {
             Debug.Log(hit.transform.name);
             Target target = hit.transform.GetComponent<Target>();
@@ -61,8 +56,8 @@ public class Gun : MonoBehaviour
 
         void shootProjectile()
     {
-        GameObject ball = Instantiate(projectile, barrelEnd.transform.position, transform.rotation);
-        ball.GetComponent<Rigidbody>().AddForce(barrelEnd.transform.forward * projectileVelocity,ForceMode.Impulse);
+        GameObject ball = Instantiate(projectile, transform.position, transform.rotation);
+        ball.GetComponent<Rigidbody>().AddForce(transform.forward * projectileVelocity,ForceMode.Impulse);
     }
 
 
